120ee47158d670811dadbcfc4e2a347fca0f7bdd
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-11-10 13:45:11 -0300

    Operadores aritmeticos

--- 4-Operadores_Aritimeticos/4-Operadores_Aritimeticos/Program.cs
+++ 4-Operadores_Aritimeticos/4-Operadores_Aritimeticos/Program.cs
@@ -6,7 +6,15 @@ namespace _4_Operadores_Aritimeticos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a = 1.0, b = -3.0, c = -4.0;
+
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+
+            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+            Console.WriteLine(delta);
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
         }
     }
 }
