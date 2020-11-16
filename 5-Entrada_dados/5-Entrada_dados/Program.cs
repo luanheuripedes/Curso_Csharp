eb460bdaa47b0f39e2850e527da60c3e5db462de
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-11-16 15:49:02 -0300

    Entrada e saida de dados em csharp

---
+++ 5-Entrada_dados/5-Entrada_dados/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace _5_Entrada_dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Comando de entrada de dados Console.ReadLine()
+
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            */
+
+            //Split
+
+            /*
+            //string[] v = Console.ReadLine().Split(' ');
+            string s = Console.ReadLine();
+            string[] vet = s.Split(' ');
+
+            string a = vet[0];
+            string b = vet[1];
+            string c = vet[2];
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            */
+
+            /*
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2  = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+
+
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine(nome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(sexo);
+            */
+
+            Console.WriteLine("Entre com seu nome completo:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int quartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o preço do produto:");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre seu ultimo nome, idade e altura");
+            string[] vet = Console.ReadLine().Split(' ');
+            string ultimo = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(ultimo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
