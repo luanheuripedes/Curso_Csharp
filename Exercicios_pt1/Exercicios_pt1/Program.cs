19a6d0c34a15b970ac31c00a676183e379deab2e
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-11-29 20:32:34 -0300

    Ex1

--- Exercicios_pt1/Exercicios_pt1/Program.cs
+++ Exercicios_pt1/Exercicios_pt1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Exercicios_pt1
 {
@@ -6,7 +7,30 @@ namespace Exercicios_pt1
     {
         static void Main(string[] args)
         {
-            
+            /*
+            //Ex1
+            Console.WriteLine("Digite o primeiro valor");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor");
+            int b = int.Parse(Console.ReadLine());
+
+            int soma = a + b;
+
+            Console.WriteLine($"Soma:{soma}");
+            */
+
+            //Ex2
+
+            double pi = 3.14159f,raio,area;
+
+            Console.WriteLine("Digite o raio do circulo:");
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = Math.Pow(raio,2.0) * pi;
+
+            Console.WriteLine($"A={area:f4}");
+
         }
     }
 }
