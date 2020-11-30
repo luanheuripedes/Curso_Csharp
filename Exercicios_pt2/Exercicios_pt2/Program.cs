78b4211126529cdf5397947d780101607370dced
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-11-30 10:00:13 -0300

    Ex1-Ex2

---
+++ Exercicios_pt2/Exercicios_pt2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercicios_pt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex1
+            /*
+            int number = int.Parse(Console.ReadLine());
+
+            if(number >= 0)
+            {
+                Console.WriteLine("Não negativo");
+            }
+            else
+            {
+                Console.WriteLine("Negativo");
+            }
+            */
+
+            //Ex2
+            /*
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+            */
+
+            //
+        }
+    }
+}
