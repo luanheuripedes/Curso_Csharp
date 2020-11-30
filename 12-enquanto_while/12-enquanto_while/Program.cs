b95ca512a5958a2e697fad894339b237d199d809
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-11-30 20:28:57 -0300

    While(enquanto)

---
+++ 12-enquanto_while/12-enquanto_while/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace _12_enquanto_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (x >= 0.0)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("f3", CultureInfo.InvariantCulture));
+                Console.Write("Digite outro número ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            
+        }
+    }
+}
