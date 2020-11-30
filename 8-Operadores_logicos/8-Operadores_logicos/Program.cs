5fdc3d64d5ea9a165d9af6fd027fc6e9045905dc
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-11-29 22:10:33 -0300

    Operadores Logicos

---
+++ 8-Operadores_logicos/8-Operadores_logicos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _8_Operadores_logicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool c1 = 2 > 3 &&  4 != 5;
+
+            bool c2 = !(2 > 3) || 4 != 5;
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+
+            Console.WriteLine("---------------");
+
+            bool c3 = 10 < 5;
+            bool c4 = c1 || c2 && c3;
+
+            Console.WriteLine(c3);
+            Console.WriteLine(c4);
+        }
+    }
+}
