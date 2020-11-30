c2f2d9f7f5f912eea44f7bb25ebc1f9ebccdae3f
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2020-11-30 09:49:49 -0300

    Escopo

---
+++ 10-escopo_Inicializacao/10-escopo_Inicializacao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10_escopo_Inicializacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine());
+            double desconto = 0.0;
+
+            if(preco > 100)
+            {
+                desconto = preco * 0.1;
+            }
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
